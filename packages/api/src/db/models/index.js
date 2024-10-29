e0ae41b8150d7319b0432e59ebacf108bb7aa326
haorui <haorui215@126.com>
2024-10-29 12:55:52 +0800

    feat(api): update db init

--- packages/api/src/db/models/index.js
+++ packages/api/src/db/models/index.js
@@ -4,31 +4,39 @@ const { Sequelize, DataTypes } = require('sequelize');
 const basename = path.basename(__filename);
 
 const env = process.env.NODE_ENV;
-console.log('env ', env);
+console.log('NODE_ENV: ', env);
 
-console.log('env base url: ', process.env.ONLINE_DATABASE_URL);
 const db = {};
-const sequelize = new Sequelize(process.env.ONLINE_DATABASE_URL, {
-  dialect: 'mysql',
-  logging: env === 'production' || env === 'production_self' ? false : true,
-});
-
-fs.readdirSync(__dirname)
-  .filter(file => {
-    return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, DataTypes);
-    db[model.name] = model;
+function init() {
+  const dbUri = process.env.DB_URL.replace('java:', '');
+  console.log('DB db uri: ', dbUri);
+  const pwd = fs.readFileSync(process.env.DB_PASSWORD_FILE, 'utf-8');
+  const sequelize = new Sequelize(dbUri, {
+    username: process.env.DB_USER,
+    password: pwd,
+    dialect: 'mysql',
+    logging: env === 'production' || env === 'production_self' ? false : true,
   });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+  fs.readdirSync(__dirname)
+    .filter(file => {
+      return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
+    })
+    .forEach(file => {
+      const model = require(path.join(__dirname, file))(sequelize, DataTypes);
+      db[model.name] = model;
+    });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+  Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+      db[modelName].associate(db);
+    }
+  });
+
+  db.sequelize = sequelize;
+  db.Sequelize = Sequelize;
+}
+
+init();
 
 module.exports = db;
